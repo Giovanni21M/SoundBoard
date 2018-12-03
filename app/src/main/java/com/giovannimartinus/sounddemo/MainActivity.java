04daf4be21d35d0b248a2510e05cfadd896abd6b
Giovanni21M <giovanni21martinus@gmail.com>
2018-12-02 22:05:46 -0500

    DEBUG: muteButton Icon Remains The Same When SeekBar is Moved

--- app/src/main/java/com/giovannimartinus/sounddemo/MainActivity.java
+++ app/src/main/java/com/giovannimartinus/sounddemo/MainActivity.java
@@ -112,28 +112,31 @@ public class MainActivity extends AppCompatActivity {
             int volume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
             int third = volume / 3;
 
+            if (muted == false) {
+                // if the x is between volume - 1/3 or itself
+                if (x == volume || x >= (volume - third)) {
+                    volumeButton.setImageResource(R.drawable.highvolume);
+                    muteButton.setImageResource(R.drawable.highvolume);
+                    muteButton.setTag(0);
+                    // else if the x is at 0
+                } else if (x == 0) {
+                    volumeButton.setImageResource(R.drawable.novolume);
+                    muteButton.setImageResource(R.drawable.novolume);
+                    muteButton.setTag(1);
+                    // else if x is between 1 and 1/3 of volume
+                } else if (x >= 1 && x < third) {
+                    volumeButton.setImageResource(R.drawable.lowvolume);
+                    muteButton.setImageResource(R.drawable.lowvolume);
+                    muteButton.setTag(2);
+                    // else if x is between 1/3 of volume and volume - 1/3
+                } else if (x < (volume - third) && x >= third) {
+                    volumeButton.setImageResource(R.drawable.mediumvolume);
+                    muteButton.setImageResource(R.drawable.mediumvolume);
+                    muteButton.setTag(3);
+                }
+            } // what happens when having no alternative clause
 
-            // if the x is between volume - 1/3 or itself
-            if (x == volume || x >= (volume - third)) {
-                volumeButton.setImageResource(R.drawable.highvolume);
-                muteButton.setImageResource(R.drawable.highvolume);
-                muteButton.setTag(0);
-            // else if the x is at 0
-            } else if (x == 0) {
-                volumeButton.setImageResource(R.drawable.novolume);
-                muteButton.setImageResource(R.drawable.novolume);
-                muteButton.setTag(1);
-            // else if x is between 1 and 1/3 of volume
-            } else if (x >= 1 && x < third) {
-                volumeButton.setImageResource(R.drawable.lowvolume);
-                muteButton.setImageResource(R.drawable.lowvolume);
-                muteButton.setTag(2);
-            // else if x is between 1/3 of volume and volume - 1/3
-            } else if (x < (volume - third) && x >= third) {
-                volumeButton.setImageResource(R.drawable.mediumvolume);
-                muteButton.setImageResource(R.drawable.mediumvolume);
-                muteButton.setTag(3);
-            }
+
         }
 
         // allow volume control with SeekBar widget
